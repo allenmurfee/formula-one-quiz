be4d0be9872d842fb99e042fb384b5ae90efadd5
allenmurfee <allenmurfee123@gmail.com>
2022-09-03 18:08:04 -0400

    added JS to hide questions on initial page load

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 var startButton = document.querySelector("#start-button");
 var timerEl = document.querySelector("#timer");
 var correctAnswer = document.querySelector("#correct");
+var questions = document.querySelector(".question");
 var questionOne = document.querySelector("question-one");
 var questionTwo = document.querySelector("question-two");
 var questionThree = document.querySelector("question-three");
 var questionFour = document.querySelector("question-four");
 var questionFive = document.querySelector("question-five");
 
-var totalQuestions = ".questions".size();
+// var totalQuestions = ".questions".size();
 var score = 0;
 
+document.getElementById("question-one").style.display = "none";
+document.getElementById("question-two").style.display = "none";
+document.getElementById("question-three").style.display = "none";
+document.getElementById("question-four").style.display = "none";
+document.getElementById("question-five").style.display = "none";
 
 // Function for quiz
 function startQuiz() {
@@ -20,15 +26,14 @@ function startQuiz() {
     // go to question 2
     score++;
     // Need to add "correct answer"
-   } else {
+  } else {
     score--;
     timerCount = timerCount - 5;
-}
+  }
 
-
-if (timerCount === 0) {
+  if (timerCount === 0) {
     return;
-}
+  }
 }
 
 // Function for timer
@@ -39,6 +44,5 @@ function startTimer() {
     // need to add if a question is wrong, subtract 5
   }, 1000);
 }
-
 
 startButton.addEventListener("click", startQuiz);
